c819f61e600d0ce38d5717b30b4f2e0179527e74
HFCherish <hf_cherish@163.com>
2018-01-19 17:47:33 +0800

    loan should use chain constructor

--- src/main/java/com/tw/refactoring/templatemethod/Loan.java
+++ src/main/java/com/tw/refactoring/templatemethod/Loan.java
@@ -16,12 +16,25 @@ public final class Loan {
 
 //    创建一个定期贷款（term loan）
     public Loan(double commitment, int riskRating, LocalDate maturity) {
-        this(commitment, riskRating, maturity, null);
+//        this(commitment, riskRating, maturity, null);
+        this.commitment = commitment;
+        this.riskRating = riskRating;
+        this.maturity = maturity;
+        outstanding = 0;
+        expire = null;
+        capitalStrategy = null;
     }
 
 //    创建一个循环贷款（revolving loan）
     public Loan(double commitment, int riskRating, LocalDate maturity, LocalDate expire) {
-        this(commitment, riskRating, 0.00, maturity, expire);
+//        this(commitment, riskRating, 0.00, maturity, expire);
+        this.commitment = commitment;
+        this.riskRating = riskRating;
+        this.maturity = maturity;
+        this.expire = expire;
+
+        capitalStrategy = null;
+        outstanding = 0;
     }
 
 //    创建一个循环定期贷款
@@ -31,7 +44,13 @@ public final class Loan {
                 LocalDate maturity,
                 LocalDate expire) {
 
-        this(null, commitment, riskRating, outstanding, maturity, expire);
+//        this(null, commitment, riskRating, outstanding, maturity, expire);
+        this.commitment = commitment;
+        this.riskRating = riskRating;
+        this.outstanding = outstanding;
+        this.maturity = maturity;
+        this.expire = expire;
+        capitalStrategy = null;
     }
 
 //    创建一个循环定期贷款，采用特定的风险调整的资本策略
