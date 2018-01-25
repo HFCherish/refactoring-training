49e8dc4a551b207a4cb360fc38523de958da0f7f
HFCherish <hf_cherish@163.com>
2018-01-25 18:07:39 +0800

    [encapsulate field] add static field

--- src/test/java/com/tw/refactoring/EncapsulateFieldTest.java
+++ src/test/java/com/tw/refactoring/EncapsulateFieldTest.java
@@ -1,10 +1,19 @@
 package com.tw.refactoring;
 
+import org.junit.Test;
+
 /**
  * @author pzzheng
  * @date 1/25/18
  */
 public class EncapsulateFieldTest {
+    /**
+     * @author pzzheng
+     */
+    @Test
+    public void test() {
+        System.out.println(PersonFactory.CHINA_POPULATION);
+    }
 }
 
 class Person {
@@ -18,6 +27,8 @@ class Person {
 }
 
 class PersonFactory {
+    public static double CHINA_POPULATION = 13.79;
+
     public static Person simplePerson(String name, String age) {
         Person person = new Person();
         person.name = name;
